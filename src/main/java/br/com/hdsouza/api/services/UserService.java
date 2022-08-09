061e9220db9dbfe44513f3599139a4b5afc27f69
Helder Souza <helder825@hotmail.com>
2022-08-09 14:57:06 -0300

    endpoint findAll para user

--- src/main/java/br/com/hdsouza/api/services/UserService.java
+++ src/main/java/br/com/hdsouza/api/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.hdsouza.api.services;
 
 import br.com.hdsouza.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
